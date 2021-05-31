198919da5a270b6da73633055adac51f74e83f9b
Your Name <you@example.com>
2021-05-31 08:13:41 +0700

    SS

--- Main.js
+++ Main.js
@@ -94,8 +94,8 @@ if(String(keytemp) === String(keyx)){
         async function enGine(){
             if(Number(kstart) != Number(start)){
                 counter = counter + 1;
-                if(counter > 7) await new Promise(resolve => setTimeout(resolve, 4700));
-                else await new Promise(resolve => setTimeout(resolve, 2100));
+                if(counter > 7) await new Promise(resolve => setTimeout(resolve, 4300));
+                else await new Promise(resolve => setTimeout(resolve, 1900));
             }
 
             let check = false;
