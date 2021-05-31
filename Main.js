77d1114bdaf6433fb756d9e824d033e257ed6635
Your Name <you@example.com>
2021-05-31 18:28:53 +0700

    SS

--- Main.js
+++ Main.js
@@ -97,9 +97,9 @@ if(String(keytemp) === String(keyx)){
         async function enGine(){
             if(Number(kstart) != Number(start)){
                 counter = counter + 1;
-                if(counter < 7) await new Promise(resolve => setTimeout(resolve, 1500));
-                else if(counter >= 7 && counter < 33) await new Promise(resolve => setTimeout(resolve, 2500));
-                else if(counter >= 33 && counter < 77) await new Promise(resolve => setTimeout(resolve, 3500));
+                if(counter < 7) await new Promise(resolve => setTimeout(resolve, 1900));
+                else if(counter >= 7 && counter < 23) await new Promise(resolve => setTimeout(resolve, 2900));
+                else if(counter >= 23 && counter < 33) await new Promise(resolve => setTimeout(resolve, 3900));
                 else exit = false;
             }
 
